74e833bf778aee894b4f3f60fad044a90f52fcde
xpextro93 <jonathanandrade@pursuit.org>
2020-11-12 19:43:24 -0500

    add functionality to function onFileChange

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const { Storage } = require('@google-cloud/storage');
+const projectId = 'upload-80d65';
+let gcs = new Storage ({
+  projectId
+});
+const os = require('os');
+const path = require('path');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
+exports.onFileChange = functions.storage.object().onFinalize(event => {
+  console.log(event);
+  const bucket = event.bucket;
+  const contentType = event.contentType;
+  const filePath = event.name;
+  console.log('file detected')
+
+if(path.basename(filePath).startsWith('renamed-')){
+  console.log('already renamed this file')
+  return;
+}
+
+  const destBucket = gcs.bucket(bucket);
+  const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+  const metadata = { contentType : contentType }
+  return destBucket.file(filePath).download({
+    destination : tmpFilePath
+  }).then(() => {
+   return destBucket.upload(tmpFilePath, {
+    destination:'renamed-'+ path.basename(filePath),
+    metadata: metadata
+  }) 
+
+ 
+  })
+
+});
